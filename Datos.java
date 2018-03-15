ffb2c95783340b5eb7d57b40df43e131e509784b
Rodolfo Salgado <37157142+AlexanderSalgado@users.noreply.github.com>
2018-03-14 21:57:40 -0600

    Add files via upload

---
+++ Datos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author rodol
+ */
+public class Datos {
+  public static void main(String[] args) {
+        System.out.println("Nombre:Rodolfo");
+        System.out.println("Apellido:Salgado");
+        System.out.println("Carrera: Ing. Informatica");
+        System.out.println("Carne:00191617");
+        System.out.println("Fecha de nacimiento: 28/02/99");
+        
+  }
+}
+ 
+
